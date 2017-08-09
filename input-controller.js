ce316d8973541a50448f36bda9cdba6348754879
Laura Senko <laurasenko@gmail.com>
2017-08-09 12:56:24 -0500

    commite faied 1st time

--- input-controller.js
+++ input-controller.js
@@ -5,7 +5,7 @@ var app = angular.module("madLibsApp");
 // Define a controller named 'inputController' 
 app.controller("inputController", function($scope, madLibsService) {
 	//initializes empty array
-	$scope.words = [];   
+	$scope.words = {};   
     // Add a variable called "setInput" to the scope.
     $scope.setInput = function() {
     	madLibsService.setInput($scope.words);
